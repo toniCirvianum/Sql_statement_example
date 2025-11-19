73d86820732120690aa028229fa065c3e2528312
Toni Fernandez <toni.fernandez@cirvianum.cat>
2025-11-19 09:10:38 +0100

    login edited

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <title>Login Vulnerable</title>
 </head>
 <body>
+    <h2>Carregar usuaris</h2>
+    <a href="addUsers.php">Afegeix usuaris a la BBDD</a>
     <h2>Login</h2>
     <form method="POST" action="login.php">
         <label>Username:</label><br>
@@ -14,5 +16,39 @@
 
         <button type="submit">Login</button>
     </form>
+    <hr>
+    <div>
+        <p>
+            <a href="">Mostra tots els usuaris</a>
+        </p>
+    </div>
+    <hr>
+    <h2> Selecciona usuari per Id</h2>
+    <form method="POST" action="getById.php">
+        <label>Id:</label><br>
+        <input type="text" name="id_user"><br><br>
+
+        <button type="submit">Envia</button>
+    </form>
+    <hr>
+        <h2> Elimina usuari per Id</h2>
+    <form method="POST" action="DelById.php">
+        <label>Id:</label><br>
+        <input type="text" name="id_user"><br><br>
+
+        <button type="submit">Envia</button>
+    </form>
+    <hr>
+        <h2> Actualitza usuari per Id</h2>
+    <form method="POST" action="UpdateById.php">
+        <label>Id:</label><br>
+        <input type="text" name="id_user"><br><br>
+
+        <button type="submit">Envia</button>
+    </form>
+
+
+
+
 </body>
 </html>
